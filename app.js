d00d23c15cc4ca76bb7224deda4ed1ddbac30b6a
twdharani <dharani.thangavel@thoughtworks.com>
2024-09-01 22:04:01 +0530

    Add. sending responses

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const http = require("http");
 const { request } = require("https");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  // process.exit;
+    console.log(req.url, req.method, req.headers);
+    // process.exit;
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My Node JS page</title></head>');
+    res.write('<body><h1>Hey, This is my Node Js page</h1></body>');
+    res.write("</html>");
 });
 
 server.listen(3000);
