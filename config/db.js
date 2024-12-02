b5d95b67d7c920ceafa500775213d6c064151c7e
Rani-dha <gct.it18.dharani@gmail.com>
2024-12-02 15:56:53 +0530

    Add Bootcamps model and perform CRUD

--- config/db.js
+++ config/db.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-      const conn = await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
+  const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-      // Log the host after the connection is successfully established
-      console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+    // Log the host after the connection is successfully established
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+  } catch (err) {
+    // Log the error if the connection fails
+    console.error(`Mongo Error: ${err.message}`.red.bold);
+    process.exit(1); // Exit the process with a failure code
+  }
 };
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -5,18 +5,25 @@ const colors = require("colors");
 const connectDB = require("./config/db");
 
 // Load env vars
-dotenv.config({path:'./config/config.env'});
+dotenv.config({ path: "./config/config.env" });
 
 // Connect to database
 connectDB();
 
-const bootcamps = require('./routes/bootcamps');
+// Route files
+const bootcamps = require("./routes/bootcamps");
+
 const app = express();
 
+// Body parser - If it is not passed we'll get undefined in console when we console the rew body
+app.use(express.json());
+
+// Dev logging middleware
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
@@ -30,7 +37,7 @@ const server = app.listen(
 
 // Handle unhandled promise rejections
 process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error server: ${err.message}`.red);
+  console.log(`Error: ${err.message}`.red);
   // Close server & exit process
-  server.close(() => process.exit(1));
+  // server.close(() => process.exit(1));
 });
