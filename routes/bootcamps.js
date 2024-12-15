bb4544bdac416ae287fe9af4c54b1ebe853f45b2
Rani-dha <gct.it18.dharani@gmail.com>
2024-12-15 21:58:48 +0530

    Add review model, controller & route

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -10,6 +10,7 @@ const {
 
 // Include other resource routers
 const courseRouter = require('./courses');
+const reviewRouter = require("./reviews");
 const router = express.Router();
 
 const Bootcamp = require("../models/Bootcamp");
@@ -19,19 +20,21 @@ const { protect, authorize } = require("../middleware/auth");
 
 // Re-route into other resource routers
 router.use("/:bootcampId/courses", courseRouter);
- router
-   .route("/")
-   .get(advancedResults(Bootcamp, "courses"), getBootcamps)
-   .post(protect, authorize("publisher", "admin"), createBootcamp);
+router.use("/:bootcampId/reviews", reviewRouter);
 
-  router
-    .route("/:id/photo")
-    .put(protect, authorize("publisher", "admin"), bootcampPhotoUpload);
+router
+  .route("/")
+  .get(advancedResults(Bootcamp, "courses"), getBootcamps)
+  .post(protect, authorize("publisher", "admin"), createBootcamp);
 
-  router
-    .route("/:id")
-    .get(getBootcamp)
-    .put(protect, authorize("publisher", "admin"), updateBootcamp)
-    .delete(protect, authorize("publisher", "admin"), deleteBootcamp);
+router
+  .route("/:id/photo")
+  .put(protect, authorize("publisher", "admin"), bootcampPhotoUpload);
+
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(protect, authorize("publisher", "admin"), updateBootcamp)
+  .delete(protect, authorize("publisher", "admin"), deleteBootcamp);
 
 module.exports = router;
